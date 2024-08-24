dd1555b2ef19871505206936bfdc3742feb9a730
Ash French <ashfrench@users.noreply.github.com>
2024-08-24 20:57:52 +0100

    initial grid implementation

--- src/main/kotlin/org/ash/french/killer/sudoko/generators/GridFactory.kt
+++ src/main/kotlin/org/ash/french/killer/sudoko/generators/GridFactory.kt
@@ -2,32 +2,40 @@ package org.ash.french.killer.sudoko.generators
 
 import org.ash.french.killer.sudoko.domain.*
 
-interface GridFactory {
+fun defaultSudokuGrid(): SudokuGrid {
 
-    fun defaultSudokuGrid(): SudokuGrid {
+    val xRange = 1..9
+    val yRange = 1..9
 
-        val xRange = 1..9
-        val yRange = 1..9
+    val cellSet = xRange.flatMap { x ->
+        yRange.map { y -> Cell(x.toUByte(), y.toUByte())  }
+    }.toSet()
 
-        val cellSet = xRange.flatMap { x ->
-            yRange.map { y -> Cell(x.toUByte(), y.toUByte())  }
-        }.toSet()
-
-        require(cellSet.size == 45) { "Missing Cells to create a grid" }
-        return SudokuGrid(cellSet)
-    }
-
-
-    fun generateKillerSudokuGrid(): SudokuGrid
-
+    require(cellSet.size == 45) { "Missing Cells to create a grid" }
+    return SudokuGrid(cellSet)
 }
 
-data class SudokuGrid(val cells: Set<Cell>) {
+fun cageSizeToValueSets(): Map<UByte, CellSet> = TODO()
 
+fun generateEmptySudokuGrid(): SudokuGrid {
+    val cells = (1..9).flatMap { x ->
+        (1..9)
+            .map { y -> Cell(x, y) }}
+        .toSet()
+
+    return SudokuGrid(cells)
+}
+
+fun generateSudokuCells() = (1..9).flatMap { x ->
+    (1..9)
+        .map { y -> Cell(x, y) }}
+    .toSet()
+
+data class SudokuGrid(val cells: Set<Cell> = generateSudokuCells()) {
 
     val rows = cells.groupBy { it.y }.mapValues() { Row(it.key, it.value.toSet()) }
+    val columns = cells.groupBy { it.x }.mapValues() { Column(it.key, it.value.toSet()) }
 
-    val columns: Set<Column> = TODO()
     val nonets: Set<Nonet> = TODO()
 
     private val cellValues: MutableMap<Cell, UByte?> = cells.associateWith { null }.toMutableMap()
