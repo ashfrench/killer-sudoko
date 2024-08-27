4bc132837c16a259e4de6837156b0eea387cf421
Ash French <ashfrench@users.noreply.github.com>
2024-08-27 17:32:28 +0100

    SudokuGrid helper methods

--- src/main/kotlin/org/ash/french/killer/sudoko/generators/SudokuGrid.kt
+++ src/main/kotlin/org/ash/french/killer/sudoko/generators/SudokuGrid.kt
@@ -23,20 +23,24 @@ data class SudokuGrid(val cells: Set<Cell> = generateSudokuCells()) {
     fun getCellValue(x: Int, y: Int) = getCellValue(Cell(x, y))
     fun getCellValue(x: UByte, y: UByte) = getCellValue(Cell(x, y))
 
-    fun getRow(cell: Cell) = rows[cell.y]?: throw RuntimeException("Unexpected Cell: $cell - No rows found")
-    fun getRow(y: UByte) = rows[y]?: throw RuntimeException("Unexpected Y: $y - No rows found")
-    fun getRow(y: Int) = rows[y.toUByte()]?: throw RuntimeException("Unexpected Y: $y - No rows found")
+    fun getRow(cell: Cell) = rows[cell.y]?: throw RuntimeException("Unexpected Cell: $cell - No row found")
+    fun getRow(y: UByte) = rows[y]?: throw RuntimeException("Unexpected Y: $y - No row found")
+    fun getRow(y: Int) = rows[y.toUByte()]?: throw RuntimeException("Unexpected Y: $y - No row found")
 
-    fun getColumn(cell: Cell) = rows[cell.y]?: throw RuntimeException("Unexpected Cell: $cell - No rows found")
-    fun getColumn(x: UByte) = rows[x]?: throw RuntimeException("Unexpected Y: $x - No rows found")
-    fun getColumn(x: Int) = rows[x.toUByte()]?: throw RuntimeException("Unexpected Y: $x - No rows found")
+    fun getColumn(cell: Cell) = columns[cell.x]?: throw RuntimeException("Unexpected Cell: $cell - No Column found")
+    fun getColumn(x: UByte) = columns[x]?: throw RuntimeException("Unexpected Y: $x - No Column found")
+    fun getColumn(x: Int) = columns[x.toUByte()]?: throw RuntimeException("Unexpected Y: $x - No Column found")
+
+    fun getNonet(cell: Cell) = nonets.find { cell in it } ?: throw RuntimeException("No Nonet for Cell $cell")
+
+    operator fun contains(cell: Cell) = cells.contains(cell)
 
     fun updateCell(cell: Cell, value: UByte?): CellUpdate {
-        if (cellValues.containsKey(cell) ) {
-
-        } else {
-            cellValues.compute(cell) { _, oldValue -> value }
+        if (cell !in this) {
+            return CellUpdate.failure(RuntimeException("Cell $cell does not exist in this Grid"))
         }
+
+        cellValues.compute(cell) { _, oldValue -> value }
         TODO()
     }
 
