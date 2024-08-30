5de324f72d2b346fe237e7faf3557e330736fdf2
Ash French <ashfrench@users.noreply.github.com>
2024-08-30 03:30:54 +0100

    setting up the junit context store

---
+++ sudoku-solver/src/test/kotlin/org/ash/french/killer/sudoko/junit5/extensions/SudokuContextStore.kt
@@ -0,0 +1,41 @@
+package org.ash.french.killer.sudoko.junit5.extensions
+
+import org.ash.french.killer.sudoko.generators.SudokuGrid
+import org.junit.jupiter.api.extension.ExtensionContext
+import java.util.function.Function
+
+object SudokuContextStore: ExtensionContext.Store, ExtensionContext.Store.CloseableResource {
+
+    private val sudokuMapStore = mutableMapOf<Any?, Any?>()
+    private val grid: SudokuGrid = SudokuGrid()
+    override fun get(key: Any?) = sudokuMapStore[key]
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <V : Any?> get(key: Any?, requiredType: Class<V>?) = sudokuMapStore[key] as V
+
+    override fun <K : Any?, V : Any?> getOrComputeIfAbsent(key: K, defaultCreator: Function<K, V>) =
+        sudokuMapStore.computeIfAbsent(key) { _ -> defaultCreator.apply(key) }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <K : Any?, V : Any?> getOrComputeIfAbsent(
+        key: K,
+        defaultCreator: Function<K, V>,
+        requiredType: Class<V>
+    ): V = sudokuMapStore.computeIfAbsent(key) { _ -> defaultCreator.apply(key) } as V
+
+    override fun put(key: Any?, value: Any?) {
+        sudokuMapStore[key] = value
+    }
+
+    override fun remove(key: Any?) = sudokuMapStore.remove(key)
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <V : Any?> remove(key: Any?, requiredType: Class<V>?): V {
+        sudokuMapStore.remove(key)
+        return key as V
+    }
+
+    override fun close() {
+        println("Close Sudoku Context Store")
+    }
+}
--- sudoku-solver/src/test/kotlin/org/ash/french/killer/sudoko/junit5/extensions/SudokuNamespaceStore.kt
+++ sudoku-solver/src/test/kotlin/org/ash/french/killer/sudoko/junit5/extensions/SudokuNamespaceStore.kt
@@ -1,52 +1,18 @@
 package org.ash.french.killer.sudoko.junit5.extensions
 
-import org.ash.french.killer.sudoko.generators.SudokuGrid
 import org.junit.jupiter.api.extension.ExtensionContext
-import java.util.function.Function
 import kotlin.test.assertNotNull
 
-data class SudokuNamespaceStore(private val context: ExtensionContext): ExtensionContext.Store, ExtensionContext.Store.CloseableResource {
+data class SudokuNamespaceStore(private val context: ExtensionContext): ExtensionContext.Store by SudokuContextStore, ExtensionContext.Store.CloseableResource by SudokuContextStore {
 
     private val sudokuNamespace = SudokuNamespace
-    val grid: SudokuGrid = SudokuGrid()
+
     init {
         val namespace = sudokuNamespace.namespace
         val sudokuStore = context.getStore(namespace)?: throw RuntimeException("Sudoku Namespace not initialised")
         assertNotNull(sudokuStore)
         val sudokuNamespaceStore = sudokuStore.getOrComputeIfAbsent(namespace) { this } as SudokuNamespaceStore
         assertNotNull(sudokuNamespaceStore)
-    }
-
-    override fun get(key: Any?): Any {
-        TODO("Not yet implemented")
-    }
-
-    override fun <V : Any?> get(key: Any?, requiredType: Class<V>?): V {
-        TODO("Not yet implemented")
-    }
-
-    override fun <K : Any?, V : Any?> getOrComputeIfAbsent(key: K, defaultCreator: Function<K, V>?): Any {
-        TODO("Not yet implemented")
-    }
-
-    override fun <K : Any?, V : Any?> getOrComputeIfAbsent(
-        key: K,
-        defaultCreator: Function<K, V>?,
-        requiredType: Class<V>?
-    ): V {
-        TODO("Not yet implemented")
-    }
-
-    override fun put(key: Any?, value: Any?) {
-        TODO("Not yet implemented")
-    }
-
-    override fun remove(key: Any?): Any {
-        TODO("Not yet implemented")
-    }
-
-    override fun <V : Any?> remove(key: Any?, requiredType: Class<V>?): V {
-        TODO("Not yet implemented")
     }
 
     override fun close() {
