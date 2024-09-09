51e8a54532d63a60ad6f4916e0f56d41c1dce058
Ash French <ashfrench@users.noreply.github.com>
2024-09-09 11:12:12 +0100

    add spring boot test

--- spring-boot-sudoku/src/test/kotlin/org/ash/french/killer/sudoku/SudokuSolverApplicationTest.kt
+++ spring-boot-sudoku/src/test/kotlin/org/ash/french/killer/sudoku/SudokuSolverApplicationTest.kt
@@ -1,3 +1,12 @@
 package org.ash.french.killer.sudoku
 
-class SudokuSolverApplicationTest
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SudokuSolverApplicationTest {
+    @Test
+    fun `can load spring context`() {
+        assert(true) { "Spring Failed to Start" }
+    }
+}
