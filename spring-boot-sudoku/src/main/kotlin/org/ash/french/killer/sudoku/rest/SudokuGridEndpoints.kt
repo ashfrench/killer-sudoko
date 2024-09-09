8f7662229ca02f43a4bcaf3dfd33fa69da0ae6ba
Ash French <ashfrench@users.noreply.github.com>
2024-09-09 17:44:42 +0100

    Add endpoints to springboot

--- spring-boot-sudoku/src/main/kotlin/org/ash/french/killer/sudoku/rest/SudokuGridEndpoints.kt
+++ spring-boot-sudoku/src/main/kotlin/org/ash/french/killer/sudoku/rest/SudokuGridEndpoints.kt
@@ -2,16 +2,20 @@ package org.ash.french.killer.sudoku.rest
 
 import org.ash.french.killer.sudoku.domain.SudokuGrid
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 @ExperimentalUuidApi
 @RestController
 @RequestMapping("/sudoku")
 class SudokuGridEndpoints {
-    @GetMapping
-    fun getEasySudokuGrid(): SudokuGrid {
+    @GetMapping("/{id}")
+    fun getSudokuGrid(
+        @PathVariable id: Uuid,
+    ): SudokuGrid {
         TODO()
     }
 }
