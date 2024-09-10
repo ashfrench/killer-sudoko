c671a706f38e6a472bd9f93d7e09f4e558b3e3e4
Ash French <ashfrench@users.noreply.github.com>
2024-09-10 21:00:42 +0100

    test swagger is up

--- spring-boot-sudoku/src/test/kotlin/org/ash/french/killer/sudoku/SudokuSolverApplicationTest.kt
+++ spring-boot-sudoku/src/test/kotlin/org/ash/french/killer/sudoku/SudokuSolverApplicationTest.kt
@@ -2,11 +2,22 @@ package org.ash.french.killer.sudoku
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.web.client.getForEntity
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class SudokuSolverApplicationTest {
+    private val restTemplate = RestTemplateBuilder().build()
+
     @Test
     fun `can load spring context`() {
         assert(true) { "Spring Failed to Start" }
+    }
+
+    @Test
+    fun `test swagger page is up`() {
+        restTemplate.getForEntity<String>("http://localhost:8080/swagger-ui/index.html").apply {
+            assert(statusCode.is2xxSuccessful) { "Swagger Page is not up" }
+        }
     }
 }
