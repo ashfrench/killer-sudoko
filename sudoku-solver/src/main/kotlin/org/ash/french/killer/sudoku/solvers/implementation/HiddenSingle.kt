0ac6b4b8754816bafb23728c827502a9c8be1a49
Ash French <ashfrench@users.noreply.github.com>
2024-09-11 22:51:30 +0100

    example of how to create CellUpdateType

--- sudoku-solver/src/main/kotlin/org/ash/french/killer/sudoku/solvers/implementation/HiddenSingle.kt
+++ sudoku-solver/src/main/kotlin/org/ash/french/killer/sudoku/solvers/implementation/HiddenSingle.kt
@@ -1,12 +1,23 @@
 package org.ash.french.killer.sudoku.solvers.implementation
 
+import org.ash.french.killer.sudoku.domain.Cell
+import org.ash.french.killer.sudoku.domain.CellUpdate
 import org.ash.french.killer.sudoku.domain.SudokuGrid
 import org.ash.french.killer.sudoku.solvers.SudokuGridCellUpdate
+import org.ash.french.killer.sudoku.solvers.SudokuSolvingUpdateType
 import kotlin.uuid.ExperimentalUuidApi
 
 @ExperimentalUuidApi
 internal class HiddenSingle : SudokuGridCellUpdate {
     override fun getCellUpdates(grid: SudokuGrid): List<CellUpdateType> {
-        TODO("Not yet implemented")
+        val updated = CellUpdateType(
+            CellUpdate(
+                Cell(1, 1),
+                1
+            ),
+            SudokuSolvingUpdateType.HIDDEN_SINGLE,
+            "Hidden Single"
+        )
+        return listOf(updated)
     }
 }
