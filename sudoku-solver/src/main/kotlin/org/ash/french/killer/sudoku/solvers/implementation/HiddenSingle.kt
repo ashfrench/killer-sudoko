b2a28992ecf9db615194377f7c06f57d14140a28
Ash French <ashfrench@users.noreply.github.com>
2024-09-13 09:40:05 +0100

    update HiddenSingle.kt and tidy up linter

--- sudoku-solver/src/main/kotlin/org/ash/french/killer/sudoku/solvers/implementation/HiddenSingle.kt
+++ sudoku-solver/src/main/kotlin/org/ash/french/killer/sudoku/solvers/implementation/HiddenSingle.kt
@@ -1,6 +1,8 @@
 package org.ash.french.killer.sudoku.solvers.implementation
 
+import org.ash.french.killer.sudoku.domain.Column
 import org.ash.french.killer.sudoku.domain.Nonet
+import org.ash.french.killer.sudoku.domain.Row
 import org.ash.french.killer.sudoku.domain.SudokuGrid
 import org.ash.french.killer.sudoku.solvers.SudokuGridCellUpdate
 
@@ -8,16 +10,17 @@ internal class HiddenSingle : SudokuGridCellUpdate {
     override fun getCellUpdates(grid: SudokuGrid): List<CellUpdateType> {
         val updates =
             (1..9)
-                .flatMap { nonetPosition ->
-                    (1..9).mapNotNull { hiddenSingleCheck ->
-                        val nonet = grid.getNonet(nonetPosition)
-                        val update =
-                            nonet
-                                .findHiddenSingle(
-                                    grid,
-                                    hiddenSingleCheck,
-                                )
-                        update
+                .flatMap { hiddenSingleCheck ->
+                    (1..9).flatMap { position ->
+                        val nonet = grid.getNonet(position)
+                        val row = grid.getRow(position)
+                        val column = grid.getColumn(position)
+
+                        val nonetUpdate = nonet.findHiddenSingle(grid, hiddenSingleCheck)
+                        val rowUpdate = row.findHiddenSingle(grid, hiddenSingleCheck)
+                        val columnUpdate = column.findHiddenSingle(grid, hiddenSingleCheck)
+
+                        listOfNotNull(nonetUpdate, rowUpdate, columnUpdate)
                     }
                 }
 
@@ -26,6 +29,20 @@ internal class HiddenSingle : SudokuGridCellUpdate {
 }
 
 private fun Nonet.findHiddenSingle(
+    grid: SudokuGrid,
+    hiddenSingleCheck: Int,
+): CellUpdateType? {
+    return null
+}
+
+private fun Row.findHiddenSingle(
+    grid: SudokuGrid,
+    hiddenSingleCheck: Int,
+): CellUpdateType? {
+    return null
+}
+
+private fun Column.findHiddenSingle(
     grid: SudokuGrid,
     hiddenSingleCheck: Int,
 ): CellUpdateType? {
