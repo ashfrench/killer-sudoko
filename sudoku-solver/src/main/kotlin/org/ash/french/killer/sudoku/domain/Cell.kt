ffdb6b01c884ca22134252693157deb4933ea7fd
Ash French <ashfrench@users.noreply.github.com>
2024-09-14 19:48:42 +0100

    refactoring

--- sudoku-solver/src/main/kotlin/org/ash/french/killer/sudoku/domain/Cell.kt
+++ sudoku-solver/src/main/kotlin/org/ash/french/killer/sudoku/domain/Cell.kt
@@ -2,6 +2,15 @@ package org.ash.french.killer.sudoku.domain
 
 import kotlinx.serialization.Serializable
 
+private val xRange = (1..9)
+private val yRange = (1..9)
+val cells: Set<Cell> =
+    xRange
+        .flatMap { x ->
+            yRange
+                .map { y -> Cell(x, y) }
+        }.toSet()
+
 @Serializable
 data class Cell(val x: UByte, val y: UByte) {
     constructor(x: Int, y: Int) : this(x.toUByte(), y.toUByte())
