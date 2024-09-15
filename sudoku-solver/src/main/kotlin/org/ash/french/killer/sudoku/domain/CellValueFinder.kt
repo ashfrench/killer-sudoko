093269448a22c4eb101dd8f11c5fc9a71b019623
Ash French <ashfrench@users.noreply.github.com>
2024-09-15 13:40:08 +0100

    extract CellValueFinder to its own file

--- sudoku-solver/src/main/kotlin/org/ash/french/killer/sudoku/domain/CellValueFinder.kt
+++ sudoku-solver/src/main/kotlin/org/ash/french/killer/sudoku/domain/CellValueFinder.kt
@@ -20,29 +20,3 @@ interface CellValueFinder {
     fun getPresetCellValues(): List<CellUpdate>
 }
 
-object CellValueUpdater: CellValueFinder, CellValueSetter {
-    private val cellValues: MutableMap<Cell, UByte?> = cells.associateWith { null }.toMutableMap()
-
-    override fun getCellValue(cell: Cell): UByte? = cellValues[cell]
-
-    override fun setCellValue(
-        cell: Cell,
-        value: UByte?,
-    ): UByte? = cellValues.compute(cell) { _, _ -> value }
-
-    override operator fun contains(cell: Cell) = cells.contains(cell)
-
-    override fun getAllCellValues(): Map<Cell, UByte> {
-        val allCellValues =
-            cellValues
-                .filterValues { it != null }
-                .mapValues { it.value!! }
-
-        return allCellValues
-    }
-
-    override fun getPresetCellValues(): List<CellUpdate> {
-        TODO("Not yet implemented")
-    }
-
-}
