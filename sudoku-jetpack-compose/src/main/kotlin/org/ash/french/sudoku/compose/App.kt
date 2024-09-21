3ba6f0f4a5b68e4e57b5b280b39c95d69cee48f2
Ash French <ashfrench@users.noreply.github.com>
2024-09-21 04:31:35 +0100

    can print default grid even if ugly

--- sudoku-jetpack-compose/src/main/kotlin/org/ash/french/sudoku/compose/App.kt
+++ sudoku-jetpack-compose/src/main/kotlin/org/ash/french/sudoku/compose/App.kt
@@ -1,40 +1,54 @@
 package org.ash.french.sudoku.compose
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import org.ash.french.killer.sudoku.builders.randomDefaultGrid
-import org.ash.french.sudoku.compose.domain.nonetCard
 
 @Composable
 @Preview
 fun app() {
     val grid = randomDefaultGrid()
-    val nonets = grid.getNonets()
+    println(grid)
 
-    var text by remember { mutableStateOf(grid.toString()) }
-    println(text)
-
-    MaterialTheme {
-        Card {
-            Box {
-                (0..8).chunked(3).forEach { nonetOrder ->
-                    Row {
-                        nonetOrder.forEach { position ->
-                            val nonet = grid.getNonet(position)
-                            Column {
-                                nonetCard(nonet, grid)
-                            }
-                        }
-                    }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Sudoku Solver app bar")
+                },
+            )
+        },
+        bottomBar = {
+            BottomAppBar {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = "Sudoku Solver Bottom app bar",
+                )
+            }
+        },
+    ) { innerPadding ->
+        val modifier = Modifier.padding(innerPadding)
+        (0..8).forEach { row ->
+            val absolutePadding = modifier.absolutePadding(top = (row * 20).dp)
+            LazyRow(absolutePadding) {
+                items(9) { col ->
+                    val cellValue = grid.getCellValue(col + 1, row + 1)
+                    val cellString = cellValue ?: " "
+                    TextField(" $cellString ", onValueChange = {}, absolutePadding.requiredWidth(50.dp))
                 }
             }
         }
