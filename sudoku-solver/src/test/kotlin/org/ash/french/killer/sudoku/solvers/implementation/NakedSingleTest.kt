f95583f0baf16562dc42086d39d8523caf7fbf06
Ash French <ashfrench@users.noreply.github.com>
2024-09-23 10:45:45 +0100

    add more tests

---
+++ sudoku-solver/src/test/kotlin/org/ash/french/killer/sudoku/solvers/implementation/NakedSingleTest.kt
@@ -0,0 +1,66 @@
+package org.ash.french.killer.sudoku.solvers.implementation
+
+import org.ash.french.killer.sudoku.builders.cellValue
+import org.ash.french.killer.sudoku.builders.cellValues
+import org.ash.french.killer.sudoku.builders.sudokuGrid
+import org.ash.french.killer.sudoku.domain.CellUpdate
+import org.junit.jupiter.api.Test
+
+class NakedSingleTest {
+    @Test
+    fun `can test naked single`() {
+        val grid =
+            sudokuGrid {
+                cellValue {
+                    x = 2
+                    y = 2
+                    value = 8
+                }
+
+                cellValue {
+                    x = 4
+                    y = 2
+                    value = 1
+                }
+
+                cellValue {
+                    x = 6
+                    y = 1
+                    value = 2
+                }
+
+                cellValue {
+                    x = 6
+                    y = 3
+                    value = 9
+                }
+
+                cellValue {
+                    x = 7
+                    y = 2
+                    value = 4
+                }
+
+                cellValue {
+                    x = 8
+                    y = 2
+                    value = 5
+                }
+
+                cellValue {
+                    x = 5
+                    y = 5
+                    value = 6
+                }
+
+                cellValue {
+                    x = 6
+                    y = 5
+                    value = 7
+                }
+            }.build()
+
+        val updates = NakedSingle().getCellUpdates(grid)
+        val expectedUpdate = CellUpdate(x = 5, y = 2, 3)
+    }
+}
