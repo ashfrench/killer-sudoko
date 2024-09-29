75a35e971c996295aa37b26a08be0f0d5711cf97
Ash French <ashfrench@users.noreply.github.com>
2024-09-29 08:42:08 +0100

    create new shared common module

---
+++ share-sudoku-solver/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+}
+
+group = "org.ash.french"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
