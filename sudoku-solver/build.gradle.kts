58d905b9e08c94ba739c017aedcecaae40ca47fa
Ash French <ashfrench@users.noreply.github.com>
2024-09-29 08:17:43 +0100

    move files to commonTest

--- sudoku-solver/build.gradle.kts
+++ sudoku-solver/build.gradle.kts
@@ -11,13 +11,19 @@ repositories {
 }
 
 dependencies {
-    testRuntimeOnly("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
     // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-core
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
-
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
+}
+
+sourceSets {
+    //...
+    commonTest.dependencies {
+        implementation(libs.kotlin.test)
+        testImplementation("org.jetbrains.kotlin:kotlin-test")
+        testRuntimeOnly("org.jetbrains.kotlin:kotlin-test-junit5")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    }
 }
 
 tasks.test {
