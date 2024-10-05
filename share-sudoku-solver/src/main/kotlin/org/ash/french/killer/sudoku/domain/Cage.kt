5b8cf1ca11a833b340aff06e303abd801080bc4d
Ash French <ashfrench@users.noreply.github.com>
2024-10-05 18:48:46 +0100

    fix linter

--- share-sudoku-solver/src/main/kotlin/org/ash/french/killer/sudoku/domain/Cage.kt
+++ share-sudoku-solver/src/main/kotlin/org/ash/french/killer/sudoku/domain/Cage.kt
@@ -11,19 +11,81 @@ data class Cage(override val sum: UByte, override val cells: Set<Cell>) : Region
         potentialCageValues = generatePotentialCageValues()
     }
 
+    data class FoldingData(val cellSize: Int, val expectedSum: Int, val availableSums: MutableSet<MutableSet<Int>> = mutableSetOf())
+
     private fun generatePotentialCageValues(): Set<Set<Int>> {
-        when (cells.size) {
+        val data =
+            (9 downTo 1)
+                .fold(FoldingData(cells.size, sum.toInt())) { data, value ->
+                    val sets =
+                        data.availableSums.toMutableSet()
+                    val updatedSets =
+                        sets.map { set ->
+                            if (set.size < data.cellSize) {
+                                if (set.sum() - value >= 0) {
+                                    set.add(value)
+                                }
+                            }
+                            set
+                        }.toMutableSet()
+
+                    data.copy(availableSums = updatedSets)
+                }
+        return data.availableSums
+/*
+        return when (cells.size) {
             1 -> setOf(setOf(sum.toInt()))
-            2 -> TODO()
-            3 -> TODO()
+            2 ->
+                when (sum.toInt()) {
+                    3 -> setOf(setOf(1, 2))
+                    4 -> setOf(setOf(1, 3))
+                    5 -> setOf(setOf(1, 4), setOf(2, 3))
+                    6 -> setOf(setOf(1, 5), setOf(2, 4))
+                    7 -> setOf(setOf(1, 6), setOf(2, 5), setOf(3, 4))
+                    8 -> setOf(setOf(1, 7), setOf(2, 6), setOf(3, 5))
+                    9 -> setOf(setOf(1, 8), setOf(2, 7), setOf(3, 6), setOf(4, 5))
+                    10 -> setOf(setOf(1, 9), setOf(2, 8), setOf(3, 7), setOf(4, 6))
+                    11 -> setOf(setOf(2, 9), setOf(3, 8), setOf(4, 7), setOf(5, 6))
+                    12 -> setOf(setOf(3, 9), setOf(4, 8), setOf(5, 7))
+                    13 -> setOf(setOf(4, 9), setOf(5, 8), setOf(6, 7))
+                    14 -> setOf(setOf(5, 9), setOf(6, 8))
+                    15 -> setOf(setOf(6, 9), setOf(7, 8))
+                    16 -> setOf(setOf(7, 9))
+                    17 -> setOf(setOf(8, 9))
+                    else -> throw RuntimeException("Invalid Sum $sum for size ${cells.size}")
+                }
+            3 ->
+                when (sum.toInt()) {
+                    6 -> setOf(setOf(1, 2, 3))
+                    7 -> setOf(setOf(1, 2, 4))
+                    8 -> setOf(setOf(1, 2, 5), setOf(1, 3, 4))
+                    9 -> setOf(setOf(1, 2, 6), setOf(2, 3, 4))
+                    10 -> setOf(setOf(1, 2, 7), setOf(2, 3, 5))
+                    11 -> setOf(setOf(1, 2, 8), setOf(2, 3, 6), setOf(2, 4, 5))
+                    12 -> setOf(setOf(1, 2, 9), setOf(2, 3, 7), setOf(2, 4, 6))
+                    13 -> setOf(setOf(1, 3, 9), setOf(2, 4, 7), setOf(2, 5, 6), setOf(3, 4, 6))
+                    14 -> setOf(setOf(1, 4, 9), setOf(1, 5, 8), setOf(1, 6, 7), setOf(2, 5, 7), setOf(3, 4, 7), setOf(3, 5, 6))
+                    15 -> setOf(setOf(1, 5, 9), setOf(1, 6, 8), setOf(2, 5, 8), setOf(2, 6, 7), setOf(3, 4, 8), setOf(3, 5, 7))
+                    16 -> setOf(setOf(1, 6, 9), setOf(1, 7, 8), setOf(2, 6, 8), setOf(2, 6, 8), setOf(3, 4, 9), setOf(3, 5, 8), setOf(3, 6, 7))
+                    17 -> setOf(setOf(1, 7, 9), setOf(2, 7, 8), setOf(2, 6, 9), setOf(2, 7, 8), setOf(3, 5, 9), setOf(3, 6, 8), setOf(4, 6, 7))
+                    18 -> setOf(setOf(1, 8, 9), setOf(2, 7, 9), setOf(3, 6, 9), setOf(3, 7, 8), setOf(3, 6, 9), setOf(4, 6, 8), setOf(5, 6, 7))
+                    19 -> setOf(setOf(2, 8, 9), setOf(3, 7, 9), setOf(4, 6, 9), setOf(4, 7, 8), setOf(4, 6, 9))
+                    20 -> setOf(setOf(3, 8, 9), setOf(4, 7, 9), setOf(5, 7, 8))
+                    21 -> setOf(setOf(4, 8, 9), setOf(5, 7, 9), setOf(6, 7, 8))
+                    22 -> setOf(setOf(5, 8, 9), setOf(6, 7, 9))
+                    23 -> setOf(setOf(6, 8, 9))
+                    24 -> setOf(setOf(7, 8, 9))
+                    else -> throw RuntimeException("Invalid Sum $sum for size ${cells.size}")
+                }
             4 -> TODO()
             5 -> TODO()
             6 -> TODO()
             7 -> TODO()
             8 -> TODO()
             9 -> TODO()
+            else -> throw RuntimeException("Invalid size ${cells.size}")
         }
-        TODO("Not yet implemented")
+ */
     }
 
     private fun validateCells(cells: Set<Cell>) {
