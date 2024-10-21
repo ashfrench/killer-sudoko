81e0ed834fc769fbcad8f76b5a1efb310bf0a32c
Ash French <ashfrench@users.noreply.github.com>
2024-10-21 16:41:09 +0100

    add in app bars

--- killer-sudoku-compose/src/main/kotlin/org/ash/french/killer/sudoku/compose/Main.kt
+++ killer-sudoku-compose/src/main/kotlin/org/ash/french/killer/sudoku/compose/Main.kt
@@ -1,5 +1,12 @@
 package org.ash.french.killer.sudoku.compose
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import org.ash.french.killer.sudoku.builders.randomDefaultGrid
@@ -9,7 +16,27 @@ fun main() =
     application {
         val defaultGrid = randomDefaultGrid()
         val sudokuGridCard = SudokuGridCard(defaultGrid)
+
         Window(onCloseRequest = ::exitApplication, title = "Killer Sudoku Solver") {
-            sudokuGridCard.composableCard()
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text("Top app bar")
+                        }
+                    )
+                },
+                bottomBar = {
+                    BottomAppBar{
+                        Text(
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Center,
+                            text = "Bottom app bar",
+                        )
+                    }
+                }
+            ){ _ ->
+                sudokuGridCard.composableCard()
+            }
         }
     }
